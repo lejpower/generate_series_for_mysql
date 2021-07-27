abc1a9e865b54b96b8d1569f3bad1de55624879c
Juni <42192252+lejpower@users.noreply.github.com>
2021-07-27 19:48:42 +0900

    Update Jgenerate_series_procedure.sql

--- Jgenerate_series_procedure.sql
+++ Jgenerate_series_procedure.sql
@@ -4,10 +4,10 @@ CREATE PROCEDURE Jgenerate_series (IN max INT)
 BEGIN
   DECLARE i INT;
   SET i = 0;
-  DROP TABLE IF EXISTS Jgenerate_series_tbl;
-  CREATE TABLE Jgenerate_series_tbl (id BIGINT, date DATE, rnd_num BIGINT,rnd_float DOUBLE, rnd_str VARCHAR(255));
+  DROP TABLE IF EXISTS Jgenerate_series_DB.Jgenerate_series_tbl;
+  CREATE TABLE Jgenerate_series_DB.Jgenerate_series_tbl (id BIGINT, date DATE, rnd_num BIGINT,rnd_float DOUBLE, rnd_str VARCHAR(255));
   WHILE i < max DO
-    INSERT INTO Jgenerate_series_tbl VALUES (i, (CURDATE() - INTERVAL (FLOOR(RAND()*(100000-5+1)+5)) DAY), RAND() * max, RAND() * 10, md5(i));
+    INSERT INTO Jgenerate_series_DB.Jgenerate_series_tbl VALUES (i, (CURDATE() - INTERVAL (FLOOR(RAND()*(100000-5+1)+5)) DAY), RAND() * max, RAND() * 10, md5(i));
     SET i = i + 1;
   END WHILE;
 END
